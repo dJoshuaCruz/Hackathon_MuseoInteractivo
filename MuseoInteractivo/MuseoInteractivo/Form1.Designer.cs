8546b9d2b1058ed036627d3d7d79e28523876741
dJoshuaCruz <djoshuacruz@outlook.com>
2020-11-08 11:23:50 -0600

    init commit

---
+++ MuseoInteractivo/MuseoInteractivo/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MuseoInteractivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnStartMuseum = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnMic = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnCamara = new Bunifu.Framework.UI.BunifuImageButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelAux = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timerCamara = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStartMuseum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCamara)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelAux.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1920, 1080);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btnStartMuseum
+            // 
+            this.btnStartMuseum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(46)))), ((int)(((byte)(69)))));
+            this.btnStartMuseum.Image = ((System.Drawing.Image)(resources.GetObject("btnStartMuseum.Image")));
+            this.btnStartMuseum.ImageActive = null;
+            this.btnStartMuseum.Location = new System.Drawing.Point(0, 910);
+            this.btnStartMuseum.Name = "btnStartMuseum";
+            this.btnStartMuseum.Size = new System.Drawing.Size(1920, 170);
+            this.btnStartMuseum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnStartMuseum.TabIndex = 1;
+            this.btnStartMuseum.TabStop = false;
+            this.btnStartMuseum.Zoom = 10;
+            this.btnStartMuseum.Click += new System.EventHandler(this.btnStartMuseum_Click);
+            // 
+            // btnMic
+            // 
+            this.btnMic.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnMic.Image = ((System.Drawing.Image)(resources.GetObject("btnMic.Image")));
+            this.btnMic.ImageActive = null;
+            this.btnMic.Location = new System.Drawing.Point(0, 910);
+            this.btnMic.Name = "btnMic";
+            this.btnMic.Size = new System.Drawing.Size(350, 170);
+            this.btnMic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMic.TabIndex = 5;
+            this.btnMic.TabStop = false;
+            this.btnMic.Zoom = 10;
+            this.btnMic.Click += new System.EventHandler(this.btnMic_Click);
+            // 
+            // btnCamara
+            // 
+            this.btnCamara.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnCamara.Image = ((System.Drawing.Image)(resources.GetObject("btnCamara.Image")));
+            this.btnCamara.ImageActive = null;
+            this.btnCamara.Location = new System.Drawing.Point(1570, 910);
+            this.btnCamara.Name = "btnCamara";
+            this.btnCamara.Size = new System.Drawing.Size(350, 170);
+            this.btnCamara.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnCamara.TabIndex = 6;
+            this.btnCamara.TabStop = false;
+            this.btnCamara.Zoom = 10;
+            this.btnCamara.Click += new System.EventHandler(this.btnCamara_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 1000);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelAux
+            // 
+            this.panelAux.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(166)))), ((int)(((byte)(46)))));
+            this.panelAux.Controls.Add(this.pictureBox2);
+            this.panelAux.Location = new System.Drawing.Point(0, 910);
+            this.panelAux.Name = "panelAux";
+            this.panelAux.Size = new System.Drawing.Size(1927, 170);
+            this.panelAux.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1446, 82);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // timerCamara
+            // 
+            this.timerCamara.Interval = 3000;
+            this.timerCamara.Tick += new System.EventHandler(this.timerCamara_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.btnCamara);
+            this.Controls.Add(this.btnMic);
+            this.Controls.Add(this.panelAux);
+            this.Controls.Add(this.btnStartMuseum);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStartMuseum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCamara)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelAux.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuImageButton btnStartMuseum;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private Bunifu.Framework.UI.BunifuImageButton btnCamara;
+        private Bunifu.Framework.UI.BunifuImageButton btnMic;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelAux;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timerCamara;
+    }
+}
+
---
+++ MuseoInteractivo/MuseoInteractivo/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Newtonsoft.Json;
+using System.Speech.Synthesis;
+using SpeechSynthesizer = System.Speech.Synthesis.SpeechSynthesizer;
+using System.Globalization;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace MuseoInteractivo
+{
+    public partial class Form1 : Form
+    {
+
+        private VideoCaptureDevice MiWebCam;
+        private bool HayDispositivos;
+        private FilterInfoCollection MisDispositivios;
+        private string Path = @"D:\Hackaton\MuseoInteractivo_Entrega_Chido\MuseoInteractivo\MuseoInteractivo\Pictures\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargaDispositivos();
+            timerCamara.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/MuseoInteractivo_Entrega/MuseoInteractivo_Entrega/Banners/Inicio2.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+            btnCamara.Visible = false;
+            btnMic.Visible = false;
+            panelAux.Visible = false;
+        }
+
+        private void btnStartMuseum_Click(object sender, EventArgs e)
+        {
+            btnStartMuseum.Visible = false;
+            btnCamara.Visible = true;
+            btnMic.Visible = true;
+            panelAux.Visible = true;
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/BotonesCamaraMic2.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+        }
+
+        private async void btnMic_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/listeningSpeak.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            var speechConfig = SpeechConfig.FromSubscription("e8339b452a9d446b9b8594906389d174", "eastus");
+            var result = await FromMic(speechConfig);
+
+            btnCamara.Visible = false;
+            btnMic.Visible = false;
+
+
+            /*
+                        axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/listeningSpeak.mp4";
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        axWindowsMediaPlayer1.settings.setMode("loop", true);
+            */
+            var answer = AskQuestion(result.ToString());
+            Speech2Text(answer);
+
+
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/BotonesCamaraMic2.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            btnCamara.Visible = true;
+            btnMic.Visible = true;
+        }
+
+        async static Task<String> FromMic(SpeechConfig speechConfig)
+        {
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            var recognizer = new SpeechRecognizer(speechConfig, "es-MX", audioConfig);
+            var result = await recognizer.RecognizeOnceAsync();
+
+            return result.Text;
+        }
+        public static String AskQuestion(String Askedquestion)
+        {
+            Question question = new Question();
+            question.question = Askedquestion;
+            string result = "";
+            string data = "";
+
+            var jsonPost = JsonConvert.SerializeObject(question);
+
+            string url = "https://museoin.azurewebsites.net/qnamaker/knowledgebases/0517be53-2f85-46eb-8b7f-f44ff203e16f/generateAnswer";
+            WebRequest myReq = WebRequest.Create(url);
+            myReq.Method = "post";
+            myReq.ContentType = "application/json; charset=UTF-8";
+            myReq.Headers["Authorization"] = "EndpointKey b6bfacf3-6581-43ab-b3dd-d3f1d4b950a7";
+
+            using (var oSW = new StreamWriter(myReq.GetRequestStream()))
+            {
+                oSW.Write(jsonPost);
+                oSW.Flush();
+                oSW.Close();
+            }
+
+            WebResponse wr = myReq.GetResponse();
+            using (var oSR = new StreamReader(wr.GetResponseStream()))
+            {
+                result = oSR.ReadToEnd().Trim();
+
+                int indexStart = result.IndexOf("\"answer\":");
+                int indexStop = result.IndexOf("\"score\":");
+
+                result = result.Substring(indexStart, indexStop);
+                String answerdata = result.Substring(9, result.Length - 9);
+                indexStop = answerdata.IndexOf("\"score\":");
+                data = answerdata.Substring(0, indexStop);
+                oSR.Close();
+            }
+
+            return data;
+        }
+        public void Speech2Text(String text)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child, 0, CultureInfo.GetCultureInfo("es-MX"));
+            synthesizer.Speak(text);
+        }
+        class Question
+        {
+            public string question { get; set; }
+
+            public override string ToString()
+            {
+                return $"{question}";
+            }
+        }
+
+        private void btnCamara_Click(object sender, EventArgs e)
+        {
+            
+            axWindowsMediaPlayer1.Visible = false;
+            CerrarWebCam();
+            string NombreVideo = MisDispositivios[0].MonikerString;
+            MiWebCam = new VideoCaptureDevice(NombreVideo);
+            MiWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            MiWebCam.Start();
+            timerCamara.Start();
+        }
+
+        private void CerrarWebCam()
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+            }
+        }
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Imagen;
+        }
+        public void CargaDispositivos()
+        {
+            MisDispositivios = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MisDispositivios.Count > 0)
+            {
+                HayDispositivos = true;
+                for (int i = 0; i < MisDispositivios.Count; i++)
+                    ;
+            }
+            else
+                HayDispositivos = false;
+
+        }
+        private byte[] GuardaFoto()
+        {
+            byte[] ar = null;
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                ar = new byte[ms.Length];
+                ms.Write(ar, 0, ar.Length);
+            }
+            return ar;
+        }
+        private void GuardaFotoLocal()
+        {
+            try
+            {
+                if (MiWebCam != null && MiWebCam.IsRunning)
+                {
+                    pictureBox2.Image = pictureBox1.Image;
+                    pictureBox2.Image.Save(Path + "hdeleon.jpg", ImageFormat.Jpeg);
+                }
+            }
+            catch {; }
+        }
+
+
+        public static async Task<String> MakePredictionRequest(string imageFilePath)
+            {
+                var client = new HttpClient();
+                
+                // Request headers - replace this example key with your valid Prediction-Key.
+                client.DefaultRequestHeaders.Add("Prediction-Key", "c76d7b45232c4959928dd88b1194e48b");
+
+                // Prediction URL - replace this example URL with your valid Prediction URL.
+                string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/53bd8180-13d3-4329-a03a-fbe57460aeb6/classify/iterations/arqueologia/image";
+
+                HttpResponseMessage response;
+
+                // Request body. Try this sample with a locally stored image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response = await client.PostAsync(url, content);
+                   // Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            }
+            return await response.Content.ReadAsStringAsync();
+        }
+
+            private static byte[] GetImageAsByteArray(string imageFilePath)
+            {
+                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+
+        public static async Task<String> predictPhoto(byte[] byteData)
+        {
+            var client = new HttpClient();
+            // Request headers - replace this example key with your valid Prediction-Key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "c76d7b45232c4959928dd88b1194e48b");
+
+            // Prediction URL - replace this example URL with your valid Prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/53bd8180-13d3-4329-a03a-fbe57460aeb6/classify/iterations/arqueologia/image";
+
+            HttpResponseMessage response;
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                
+            }
+
+            return response.ToString();
+        }
+        private async void timerCamara_Tick(object sender, EventArgs e)
+        {
+            GuardaFotoLocal();
+            String jsonStr = "";
+          string path = @"D:\Hackaton\MuseoInteractivo_Entrega_Chido\MuseoInteractivo\MuseoInteractivo\Pictures\";
+          jsonStr = await MakePredictionRequest(path + "hdeleon.jpg");
+
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/listeningSpeak.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            pictureBox1.Visible = false;
+            axWindowsMediaPlayer1.Visible = true;
+
+            var myJsonString = jsonStr;
+            var jo = JObject.Parse(myJsonString);
+            var predicion = jo["predictions"][0]["tagName"].ToString();
+          //  MessageBox.Show(predicion);
+            CerrarWebCam();
+
+          
+
+
+            var answer = AskQuestion(predicion);
+            Speech2Text(answer);
+
+
+            axWindowsMediaPlayer1.URL = "D:/Hackaton/InnovaccionVirtual/CodigoCS/InteractiveMuseum/InteractiveMuseum/InteractiveMuseum/Banners/BotonesCamaraMic2.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            timerCamara.Stop();
+
+        }
+    }
+}
+
